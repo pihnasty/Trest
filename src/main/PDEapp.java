125299921af8630bf19ca884426665e44d188abe
Pihnastyi Oleg  E <innov@bk.ru>
2016-02-07 21:01:32 +0200

    Add itemmenu TEST.

--- src/main/PDEapp.java
+++ src/main/PDEapp.java
@@ -9,6 +9,5 @@ import ui.MainApplicationFX;
 public class PDEapp {
     public static void main(String[] args) {
        MainApplicationFX uiApp = new MainApplicationFX(args);
-        //--
     }
 }
