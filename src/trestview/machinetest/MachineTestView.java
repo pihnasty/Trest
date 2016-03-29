8923a3dfcb95f0b95147cd103d2f568ac7584aea
Korsun Roman <roman.korsun7@gmail.com>
2016-03-29 20:05:12 +0300

    1. added spit panel in MachineTestView 2. animation of manufacturing

--- src/trestview/machinetest/MachineTestView.java
+++ src/trestview/machinetest/MachineTestView.java
@@ -7,6 +7,8 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -43,16 +45,29 @@ public class MachineTestView extends BorderPane implements Observer {
 
 //        setTitle(fxmlLoader.getResources().getString("TestOfMachine"));
 
-//        VBox vBox = new VBox();
-//        vBox.setSpacing(5);
-//        vBox.setPadding(new Insets(10, 5, 5, 10));
 
+        VBox vBox = new VBox();
+        vBox.setSpacing(5);
+        vBox.setPadding(new Insets(10, 5, 5, 10));
+
+        //img
+        Image image = new Image("resources/images/animation/2.gif");
+
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+//        imageView.fitWidthProperty().bind(widthProperty());
+
+        imageView.setPreserveRatio(true);
+
+        vBox.getChildren().addAll(new Label("Hello"), imageView);
 //        getDialogPane().setContent(vBox);
 //        getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 ////
 //
 //
 //        Optional<Pair<String, Boolean>> result = showAndWait();
+        SplitPane splitPane = new SplitPane();
+
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("X");
@@ -68,10 +83,11 @@ public class MachineTestView extends BorderPane implements Observer {
         populateSeries(machineTestModel.getRandomValuesList());
 
 
+        splitPane.getItems().addAll(vBox,lineChart);
 
 
 //        getChildren().add(lineChart);
-            setCenter(lineChart);
+            setCenter(splitPane);
     }
 
     private void populateSeries(ArrayList<Double> list) {
